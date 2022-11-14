967b3acfeaea607425a185223c126ab3ad7140c2
Jeffrey Wang <jwjeff200@gmail.com>
2022-11-14 15:51:21 -0500

    Adding some info for population

--- assets/js/comparison-page.js
+++ assets/js/comparison-page.js
@@ -1,10 +1,46 @@
 // populate content based on the searched username
 function populate_col(username) {
   if (username == "jasminebrew") {
+    // no solo, B2 flex
+    //Escapist - green
+    //Pacifist - yellow
+    //Visionless - red
+    //sup 25% vs 100% opacity on no-sup vs sup
+    //seraphine 34 - 50%wr - 1.5/6.6/8.7 - 1.55kda
+    //jinx 27 - 44%wr - 3.4/7.0/6.9 - 1.47kda
+    //ashe 22 - 45%wr - 1.0/6.5/9.7 - 1.63kda
+    // Tyv 56wr 18games, Snow 50wr, 6games
   } else if (username == "qotato") {
+    //G3 solo, no flex
+    //Midgame Master - green
+    //Lane tyrant - green
+    //Late bloomer - green
+    //top, bot
+    //teemo 15 - 53%wr - 6.3/5.7/5.9 - 2.1kda
+    //samira 11 - 36%wr - 8.3/5.1/5.1 - 2.6kda
+    //fiora 4 - 75%wr - 4.0/4.3/3.5 - 1.8kda
   } else if (username == "Boxerme") {
+    //
   } else if (username == "ElegantDisaster") {
+    // G2 solo, no flex
+    //Early Warder - green
+    //Vision focused - green
+    //Risk blind - red
+    // bot, sup
+    // zyra 61 - 62wr 3.3/6.3/11.5 - 2.33kda
+    // yuumi 52 - 56wr 2.4/4.5/17.0 - 4.3kda
+    // MF 39 - 51wr 4.8/5.9/8.2 - 2.19kda
+    // Akuma Tan 50wr 12 game, SereneCrow 50wr 4 game
   } else if (username == "Tyv") {
+    // D4 solo, G2 flex
+    //Deadly Duelist - green
+    //Solid Laner - green
+    //Ultimate Predator - green
+    // mid, bot
+    // zed 193 - 64wr 9.6/4.8/5.2 - 3.1kda
+    // sylas 77 - 58wr 8.1/7.3/6.9 - 1.9kda
+    // zoe 73 - 50wr 6.8/4.4/7.6 - 3.25kda
+    // jasminebrew 43wr 7 game, shiba kun 50 wr 4 game
   } else {
   }
 }
