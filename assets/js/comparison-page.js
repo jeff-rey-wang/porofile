b12e4279e4d14a0eca544817243496386e1701ea
Jeffrey Wang <jwjeff200@gmail.com>
2022-11-14 11:27:13 -0500

    Added function to populate column on comparisonpage

---
+++ assets/js/comparison-page.js
@@ -0,0 +1,10 @@
+// populate content based on the searched username
+function populate_col(username) {
+  if (username == "jasminebrew") {
+  } else if (username == "qotato") {
+  } else if (username == "Boxerme") {
+  } else if (username == "ElegantDisaster") {
+  } else if (username == "Tyv") {
+  } else {
+  }
+}
