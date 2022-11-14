83243adda4f0437db5384df0b7fb9ebabafbad08
jasonZJZhu <jasonzhu503@gmail.com>
2022-11-14 15:49:11 -0500

    Added search player interactions

--- assets/js/landing-page.js
+++ assets/js/landing-page.js
@@ -1,5 +1,10 @@
-// hardcoded data
-var players = ["Tyv", "Boxerme", "jasminebrew", "qotato", "ElegantDisaster", "Ji&#353un"]
+// hardcoded username data
+var players = ["Tyv", "Boxerme", "jasminebrew", "qotato", "ElegantDisaster", "Ji&#353un"];
+// limit to adding 5 players per search
+const addLimit = 5;
+var addedPlayers = 0;
+
+// autocomplete function
 // inp : text field element
 // arr : array of possible autocompleted values
 function autocomplete(inp, arr) {
@@ -97,3 +102,63 @@ function autocomplete(inp, arr) {
         closeAllLists(e.target);
     });
 }
+
+
+$(document).ready(function() {
+    // select region
+    $(".region-option").click(function() {
+        // alert($(this).children().text());
+        $(".search-user-text").text($(this).children().text());
+    });
+
+    // add (region, username) pair
+    $("#add_button").click(function() {
+        // check if region is selected
+        if ($(".search-user-text").text() == "Region") { alert("Please select a region"); return; }
+        // check if username is selected
+        if ($("#username_input").val() == "") { alert("Please enter a valid username"); return; }
+
+        if (addedPlayers >= addLimit) { alert("Cannot add more than 5 players"); return; }
+        else {
+            var addingPlayer = `
+            <div id="added_user_section"
+                class="added-user-container added-user-root-class-name"
+            >
+                <div class="added-user-container1">
+                    <div id="region_container" class="added-user-region-container">
+                        <span id="region_text"><span>`+$(".search-user-text").text()+`</span></span>
+                    </div>
+                    <div
+                        id="username_container"
+                        class="added-user-username-container"
+                    >
+                        <span id="username_text"><span>`+$("#username_input").val()+`</span></span>
+                    </div>
+                    <div id="trash_button" class="added-user-container2 button">
+                        <svg
+                        id="trash_icon"
+                        viewBox="0 0 804.5714285714286 1024"
+                        class="added-user-icon"
+                        >
+                        <path
+                            d="M292.571 420.571v329.143c0 10.286-8 18.286-18.286 18.286h-36.571c-10.286 0-18.286-8-18.286-18.286v-329.143c0-10.286 8-18.286 18.286-18.286h36.571c10.286 0 18.286 8 18.286 18.286zM438.857 420.571v329.143c0 10.286-8 18.286-18.286 18.286h-36.571c-10.286 0-18.286-8-18.286-18.286v-329.143c0-10.286 8-18.286 18.286-18.286h36.571c10.286 0 18.286 8 18.286 18.286zM585.143 420.571v329.143c0 10.286-8 18.286-18.286 18.286h-36.571c-10.286 0-18.286-8-18.286-18.286v-329.143c0-10.286 8-18.286 18.286-18.286h36.571c10.286 0 18.286 8 18.286 18.286zM658.286 834.286v-541.714h-512v541.714c0 27.429 15.429 43.429 18.286 43.429h475.429c2.857 0 18.286-16 18.286-43.429zM274.286 219.429h256l-27.429-66.857c-1.714-2.286-6.857-5.714-9.714-6.286h-181.143c-3.429 0.571-8 4-9.714 6.286zM804.571 237.714v36.571c0 10.286-8 18.286-18.286 18.286h-54.857v541.714c0 62.857-41.143 116.571-91.429 116.571h-475.429c-50.286 0-91.429-51.429-91.429-114.286v-544h-54.857c-10.286 0-18.286-8-18.286-18.286v-36.571c0-10.286 8-18.286 18.286-18.286h176.571l40-95.429c11.429-28 45.714-50.857 76-50.857h182.857c30.286 0 64.571 22.857 76 50.857l40 95.429h176.571c10.286 0 18.286 8 18.286 18.286z"
+                        ></path>
+                        </svg>
+                    </div>
+                </div>
+            </div>`;
+            $(".landing-page-added-players-container").append(addingPlayer);
+            addedPlayers++;
+            // clear input field 
+            $("#username_input").val("");
+        }
+    });
+
+    // remove added players
+    $(".landing-page-added-players-container").on("click", "#trash_icon", function(e) {
+        var section = $(this).closest("#added_user_section")
+        section.empty();
+        section.remove();
+        addedPlayers--;
+    });
+})
