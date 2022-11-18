783c92f1f490104da3534bc20fbc8d117f61fb43
theriauj <theriauj@mcmaster.ca>
2022-11-18 00:45:30 -0500

    old stuff gets removed if its closed for adding new user

--- assets/js/addUserPopup.js
+++ assets/js/addUserPopup.js
@@ -6,8 +6,13 @@ $('#add_new_user_button').click(()=>{
         $('#profile_placeholder').hide();
     }
     $('#add_new_user_input').val("");
+    $(".add-new-profile-popup-text02").text("Region")
 })
 
+$('#add_new_x_button').click(()=>{
+    $('#add_new_user_input').val("");
+    $(".add-new-profile-popup-text02").text("Region")
+})
 $('#banner_x_button').click(()=>{    
     $('#profile_placeholder').show();
 })
