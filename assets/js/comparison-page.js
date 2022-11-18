8dab75d3388fc3e64d7e45914930c775670a3c75
Jeffrey Wang <jwjeff200@gmail.com>
2022-11-17 23:49:10 -0500

    populated background image paths

--- assets/js/comparison-page.js
+++ assets/js/comparison-page.js
@@ -109,6 +109,7 @@ function champStats(
 
 function populate_col(username) {
   if (username == "jasminebrew") {
+    background = "assets/imgs/jasminebrew.png";
     ranks = ["none", "B2"];
     tags = [Tags.Escapist, Tags.Visionless, Tags.Pacifist];
     positions = ["sup"];
@@ -149,6 +150,7 @@ function populate_col(username) {
     ];
     playedWith = [player("Tyv", 56, 18), player("Boxerme", 50, 6)];
   } else if (username == "qotato") {
+    background = "assets/imgs/qotato.png";
     //G3 solo, no flex
     ranks = ["G3", "none"];
     //Midgame Master - green
@@ -168,6 +170,7 @@ function populate_col(username) {
     //Boxerme 70wr 10 games, ElegantDisaster 66wr 6 games
     playedWith = [player("Boxerme", 70, 10), player("ElegantDisaster", 66, 6)];
   } else if (username == "Boxerme") {
+    background = "assets/imgs/Boxerme.png";
     ranks = ["none", "B1"];
     tags = [Tags.LateBloomer, Tags.ReadyToRumble, Tags.HungryForBlood];
     positions = ["mid", "top"];
@@ -211,6 +214,7 @@ function populate_col(username) {
       player("ElegantDisaster", 55, 13),
     ];
   } else if (username == "ElegantDisaster") {
+    background = "assets/imgs/ElegantDisaster.png";
     ranks = ["G2", "none"];
     tags = [Tags.EarlyWarder, Tags.VisionFocused, Tags.Responsible];
     positions = ["bot", "sup"];
@@ -251,6 +255,7 @@ function populate_col(username) {
     ];
     playedWith = [player("qotato", 50, 12), player("Tyv", 50, 4)];
   } else if (username == "Tyv") {
+    background = "assets/imgs/Tyv.png";
     ranks = ["D4", "none"];
     tags = [Tags.DeadlyDuelist, Tags.SolidLaner, Tags.UltimatePredator];
     positions = ["mid", "bot"];
@@ -294,6 +299,5 @@ function populate_col(username) {
       player("ElegantDisaster", 50, 4),
     ];
   } else {
-    //User not found scenario
   }
 }
